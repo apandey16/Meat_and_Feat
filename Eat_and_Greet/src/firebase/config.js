d96b56f39705c767a8f55dd63535198e6cf65810
apandey16 <apande02@calpoly.edu>
2023-10-25 11:50:09 +0200

    added api key and it doesn't need to be removed

--- Eat_and_Greet/src/firebase/config.js
+++ Eat_and_Greet/src/firebase/config.js
@@ -8,6 +8,7 @@ import { getAuth } from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
+  apiKey: "AIzaSyD4zKjCCTcXn0DsjIqZhcTVl3EEUizBcoI",
   authDomain: "eatngreet-42c5d.firebaseapp.com",
   projectId: "eatngreet-42c5d",
   storageBucket: "eatngreet-42c5d.appspot.com",
