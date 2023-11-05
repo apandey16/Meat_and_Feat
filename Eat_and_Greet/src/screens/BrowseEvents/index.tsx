4268fc04a097bf6f39e2191cda212fb838c49a27
MorrowCameron <97052268+MorrowCameron@users.noreply.github.com>
2023-11-05 23:54:46 +0100

    rebased

--- Eat_and_Greet/src/screens/BrowseEvents/index.tsx
+++ Eat_and_Greet/src/screens/BrowseEvents/index.tsx
@@ -1,14 +1,84 @@
+import React, { useEffect, useState } from "react";
 import { Text, View, ScrollView } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
-
-import { Toolbar } from "../../comps/Toolbar/toolbar";
+import { db } from "../../firebase/config";
+import {
+  collection,
+  setDoc,
+  doc,
+  addDoc,
+  getDocs,
+  query,
+  where,
+} from "firebase/firestore";
 
 import styles from "../../style";
-import React from "react";
-import RoundedButton from "../../comps/RoundedButton/RoundedButton";
 import browseStyle from "./index.styles";
 
+import { Toolbar } from "../../comps/Toolbar/toolbar";
+import RoundedButton from "../../comps/RoundedButton/RoundedButton";
+
+/*
+NEXT STEPS:
+PLAN NEW EVENT BUTTON FUNCTIONALITY
+SORTED CONTAINER
+MAKE VIEW EVENT PAGE
+*/
+
+interface EventData {
+  Category: string;
+  Date: string;
+  EndTime: string;
+  Host: string;
+  StartTime: string;
+  Title: string;
+}
+
+const postData = [
+  {
+    Category: "",
+    Date: "",
+    EndTime: "",
+    Host: "",
+    StartTime: "",
+    Title: "Loading Events Now",
+  },
+];
+
+const getEventsData = async (): Promise<EventData[]> => {
+  try {
+    const docRef = await getDocs(collection(db, "Events"));
+    let fetchedEventData: EventData[] = [];
+    docRef.forEach((doc) => {
+      fetchedEventData.push(doc.data() as EventData);
+    });
+    return fetchedEventData;
+  } catch (error) {
+    console.error("Error Getting Data From DB: ", error);
+    return [];
+  }
+};
+
 export default function BrowseEvent() {
+  const [data, setData] = useState(postData);
+
+  const navigation = useNavigation();
+
+  const fetchData = async () => {
+    try {
+      const data = await getEventsData();
+      setData(data);
+    } catch (error) {
+      console.error("Error in Fetching Data: ", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <View style={styles.ScreenContainer}>
       <View style={browseStyle.InfoContainer}>
--- Eat_and_Greet/src/screens/PostEvent/index.tsx
+++ Eat_and_Greet/src/screens/PostEvent/index.tsx
@@ -1,18 +1,74 @@
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { Text, View, Image, Alert } from "react-native";
 import { StatusBar } from "expo-status-bar";
-
-import { Toolbar } from "../../comps/Toolbar/toolbar";
+import { db } from "../../firebase/config";
+import { collection, addDoc, getDocs, query, where } from "firebase/firestore";
 
 import styles from "../../style";
-import React from "react";
-import RoundedButton from "../../comps/RoundedButton/RoundedButton";
-import { Image } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context";
-
 import postStyle from "./index.styles";
+
+import { Toolbar } from "../../comps/Toolbar/toolbar";
+import RoundedButton from "../../comps/RoundedButton/RoundedButton";
 import TextBox from "../../comps/Textbox/textbox";
 
 export default function PostEvent() {
+  const [Title, setTitle] = useState("");
+  const [Category, setCategory] = useState("");
+  const [Date, setDate] = useState("");
+  const [StartTime, setStartTime] = useState("");
+  const [EndTime, setEndTime] = useState("");
+
+  const addPostToDB = async () => {
+    if (Title.length > 0) {
+      if (Category.length > 0) {
+        if (Date.length > 0) {
+          if (StartTime.length > 0) {
+            if (EndTime.length > 0) {
+              try {
+                console.log(Title, Date);
+                const querySnapshot = await getDocs(
+                  query(
+                    collection(db, "Events"),
+                    where("Title", "==", Title),
+                    where("Category", "==", Category),
+                    where("Date", "==", Date)
+                  )
+                );
+
+                if (!querySnapshot.empty) {
+                  Alert.alert("This Event is Already Happening Today");
+                } else {
+                  const docRef = await addDoc(collection(db, "Events"), {
+                    Category: Category,
+                    Date: Date,
+                    EndTime: EndTime,
+                    Host: "RobV",
+                    StartTime: StartTime,
+                    Title: Title,
+                  });
+                  console.log("Event Added with ID: ", docRef.id);
+                }
+              } catch (error) {
+                console.error("Error adding document: ", error);
+              }
+            } else {
+              Alert.alert("Please Enter an End Time");
+            }
+          } else {
+            Alert.alert("Please Enter a Start Time");
+          }
+        } else {
+          Alert.alert("Please Enter a Date");
+        }
+      } else {
+        Alert.alert("Please Enter a Category");
+      }
+    } else {
+      Alert.alert("Please Enter a Title");
+    }
+  };
+
   return (
     <SafeAreaView style={styles.ScreenContainer}>
       <StatusBar style="auto" />
@@ -24,6 +80,7 @@ export default function PostEvent() {
             top={"-80%"}
             height={"50%"}
             width={"90%"}
+            onTextChange={setTitle}
           />
         </View>
         <View style={postStyle.TitleCategoryContainer}>
@@ -33,6 +90,7 @@ export default function PostEvent() {
             top={"-80%"}
             height={"50%"}
             width={"90%"}
+            onTextChange={setCategory}
           />
         </View>
         <View style={postStyle.ImageContainer}>
@@ -51,22 +109,25 @@ export default function PostEvent() {
             top={"-30%"}
             height={"20%"}
             width={"90%"}
+            onTextChange={setDate}
           />
           <TextBox
             placeholder="Enter Start Time..."
             top={"-38%"}
             height={"20%"}
             width={"90%"}
+            onTextChange={setStartTime}
           />
           <TextBox
             placeholder="Enter End Time..."
             top={"-46%"}
             height={"20%"}
             width={"90%"}
+            onTextChange={setEndTime}
           />
         </View>
       </SafeAreaView>
-      <RoundedButton name="Post Event" top="-2%" />
+      <RoundedButton name="Post Event" top="-2%" onPress={addPostToDB} />
       <Toolbar />
     </SafeAreaView>
   );
