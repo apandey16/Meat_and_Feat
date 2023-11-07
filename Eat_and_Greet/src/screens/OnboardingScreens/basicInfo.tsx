44cb2b272ea47db9672478030ba2b58a5ac4c87e
apandey16 <apande02@calpoly.edu>
2023-11-07 12:08:47 +0100

    changed based on comments

--- Eat_and_Greet/src/screens/OnboardingScreens/basicInfo.tsx
+++ Eat_and_Greet/src/screens/OnboardingScreens/basicInfo.tsx
@@ -41,8 +41,7 @@ function BasicInfo() {
     };    
 
     const birthday = (event: any, selectedDate: any) => {
-      const currentDate = selectedDate;
-      setDob(currentDate);
+      setDob(selectedDate);
     };
     
     return (
