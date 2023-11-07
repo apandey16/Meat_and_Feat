df3fe1c40b9a5b60ab49e63e0084ffb55c80b7d9
Dylan Baurle <97052542+Logidoke@users.noreply.github.com>
2023-11-07 13:03:45 +0100

    Got rid of all code smells aside from one in App.tsx but it's not something I coded so I don't want to mess with it.

--- Eat_and_Greet/src/screens/BrowseForums/index.tsx
+++ Eat_and_Greet/src/screens/BrowseForums/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { Text, View, ScrollView, TouchableOpacity } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
