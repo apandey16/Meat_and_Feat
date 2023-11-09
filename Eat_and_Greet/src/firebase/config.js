55cbff8e81f38d54ee2cf253cdb37c1de3610e2c
apandey16 <apande02@calpoly.edu>
2023-11-09 11:28:31 +0100

    reverted a change

--- Eat_and_Greet/src/firebase/config.js
+++ Eat_and_Greet/src/firebase/config.js
@@ -22,7 +22,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const auth = getAuth(app);
+const auth = getAuth();
 const db = getFirestore(app);
 
 export { auth, db }
