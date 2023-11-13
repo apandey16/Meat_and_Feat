d541664f418e80c200646a6e87eb82be317a9581
Dylan Baurle <97052542+Logidoke@users.noreply.github.com>
2023-11-13 22:50:03 +0100

    temp

--- Eat_and_Greet/src/screens/BrowseForums/index.tsx
+++ Eat_and_Greet/src/screens/BrowseForums/index.tsx
@@ -3,10 +3,7 @@ import { Text, View, ScrollView, TouchableOpacity, Alert } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { StatusBar } from "expo-status-bar";
 import { db } from "../../firebase/config";
-import {
-  collection,
-  getDocs
-} from "firebase/firestore";
+import {collection, getDocs, query, where, setDoc} from "firebase/firestore";
 
 import styles from "../../style";
 import browseStyle from "./index.styles";
@@ -57,6 +54,53 @@ export default function BrowseForum() {
       fetchData();
     }, []);
 
+    const addForumToDB = async (ForumName: string) => {
+      let missing = new Array();
+      for (let i = 0; i < inputs.length; i++){
+        if(inputs[i].length <= 0){
+          missing.push(inputsAsString[i])
+        }
+      }
+      if (missing.length == 0) {
+        try {
+          const querySnapshot = await getDocs(
+            query(
+              collection(db, "Forums"),
+              where("Title", "==", title),
+              where("Category", "==", category),
+              where("Date", "==", dateOfEvent.toDateString())
+            )
+          );
+  
+            if (!querySnapshot.empty) {
+            Alert.alert("This Event is Already Happening Today");
+          } else {
+            const numberOfEventsSnapshot = await getDocs(collection(db, "Number of Events"));
+            let uid : number = await getNumberOfEventsData(numberOfEventsSnapshot) + 1;
+            await setDoc(doc(db, "Forums", uid.toString()), {
+              Category: category,
+              Date: dateOfEvent.toDateString(),
+              EndTime: endTime.toTimeString().split(' ')[0],
+              Host: name,
+              StartTime: startTime.toTimeString().split(' ')[0],
+              Title: title,
+              id: uid,
+              description: description,
+              spots: spots,
+              participants: [getAuth().currentUser?.email]
+            });
+            await updateDoc(doc(db, "Number of Events", "Counter"),  { numberOfEvents: uid });
+            Alert.alert("Forum Successfully Created!");
+            navigation.goBack();
+          }
+        } catch (error) {
+          console.error("Error adding document: ", error);
+        }
+      } else {
+        Alert.alert("A Forum with this name already exists.");
+      }
+    };
+
   return (
     <SafeAreaView style={styles.ScreenContainer}>
       <View style={browseStyle.InfoContainer}>
@@ -75,7 +119,8 @@ export default function BrowseForum() {
       </View>
       <RoundedButton
         name="Create Forum"
-        onPress={() => Alert.prompt('Enter Forum Name:')}
+        onPress={() => Alert.prompt('Enter Forum Name:', "", text => addForumToDB(text))}
+        top={"-2%"}
       />
       <Toolbar/>
       <StatusBar style="auto" />
