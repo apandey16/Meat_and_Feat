f16f87cc908ac0a6a8b6b4252496ab48d10ce00c
apandey16 <apande02@calpoly.edu>
2023-11-22 16:57:16 +0100

    temp

---
+++ Eat_and_Greet/src/screens/LoginScreen/loginLogic.tsx
@@ -0,0 +1,28 @@
+import {  Alert } from 'react-native';
+
+import { useNavigation } from '@react-navigation/native';
+import { auth } from '../../firebase/config';
+
+import { signInWithEmailAndPassword } from 'firebase/auth';
+
+const navigation = useNavigation();
+function loginUser(email: string, password: string) {   
+    return async () => {   
+        try {
+            await signInWithEmailAndPassword(auth, email, password);
+            console.log('User logged in successfully');
+            navigation.navigate('Home');
+          } catch (error) {
+            if (error.code === 'auth/invalid-email' || error.code === 'auth/wrong-password') {
+              Alert.alert('Your email or password was incorrect');
+            } else if (error.code === 'auth/email-already-in-use') {
+              Alert.alert('An account with this email already exists');
+            } else {
+              Alert.alert('There was a problem with your request. Please try again later.');
+            }
+          }
+    }
+
+}
+
+export default loginUser;
