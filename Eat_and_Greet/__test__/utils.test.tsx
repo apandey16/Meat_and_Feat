758b33f6c072cfa70fb3afc158ef6e8ce93cb1cc
MorrowCameron <97052268+MorrowCameron@users.noreply.github.com>
2023-11-22 20:01:32 +0100

    jest working I think Idk man I feel like I did meth and it just worked

---
+++ Eat_and_Greet/__test__/utils.test.tsx
@@ -0,0 +1,10 @@
+import EventManager from '../src/logic/EventManager';
+
+describe('Test toDateTime conversion', () => {
+  test('has 1 child', () => {
+    const EventController = new EventManager("All");
+    const input = 1000
+    const output = "Thu Jan 01 1970";
+    expect(EventController.toDateTime(input).toDateString()).toBe(output);
+  });
+});
