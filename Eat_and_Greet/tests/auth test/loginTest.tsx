d850c93049fc9f817fe778619615bda6e272f609
apandey16 <apande02@calpoly.edu>
2023-11-22 17:11:15 +0100

    testing framework

--- Eat_and_Greet/tests/auth test/loginTest.tsx
+++
@@ -1,1 +0,0 @@
-import {} from "../../src/screens/LoginScreen/index"
---
+++ Eat_and_Greet/tests/authTests/login.test.ts
@@ -0,0 +1,5 @@
+// import loginUser from "src/screens/LoginScreen/loginLogic";
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(1+2).toBe(3);
+  });
